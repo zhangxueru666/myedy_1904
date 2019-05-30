e90639836ada722af73c686830b0e69e8b3e4ca0
雪茹 <1023849053@qq.com>
2019-05-30 10:59:57 +0800

    first commit

---
+++ day01/__init__.py
@@ -0,0 +1,26 @@
+from day01 import base_type
+
+
+
+def float_demo():
+    afloat = 1.1
+    print(type(afloat))
+    print('2')
+
+def def1():
+    print('def1')
+
+def def2():
+    print('def2')
+
+def def3():
+    print('def3')
+
+
+if __name__ == '__main__':
+    def1()
+    def2()
+    def3()
+    float_demo()
+    value = base_type.add(80,80)
+    print(value)
---
+++ day01/base_type.py
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+def int_demo():
+    aint = 5
+    print(type(aint))
+
+def  str_demo():
+    astr='5 '
+    print(type(astr))
+def float_demo():
+    afloat = 1.1
+    print(type(afloat))
+    print('2')
+
+
+def type_xueru ():
+    b ='100'
+    print(type(b))
+    b = int(b)
+    print(type(b))
+def type_baobao ():
+    a = 80
+    print(type(a))
+    a =str(a)
+
+
+def str_join():
+    a = 222
+    b = '嘿嘿'
+    c = '大大'
+    print(b+c)
+    print(str(a)+b )
+    print('a %s , b %s ,c %s'%(a,b,c))
+
+
+
+
+def add(num1,num2):
+    print(num1)
+    print(num2)
+    print(num1+num2)
+    return num1+num2
+
+if __name__ == '__main__':
+
+ b = '开开心心快快乐乐'
+ print(b[0:])
+ print(b[1:3])
+ print(b[3:5])
+ print(b[::-3])
